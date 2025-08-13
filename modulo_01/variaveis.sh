083aa72528f833743afe30b26f6ec8bde38cf3c3
Lucas Safeno <lugnorio@gmail.com>
2025-08-13 19:26:42 -0300

    #A05 005 Variaveis

--- modulo_01/variaveis.sh
+++ modulo_01/variaveis.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env/bash
 
-echo 'Teste'
+NOME="Lucas
 
+Tenório"
+
+NUMERO_1=24
+NUMERO_2=45
+
+TOTAL=$((NUMERO_1+NUMERO_2))
+
+SAIDA_CAT="$(cat /etc/passwd | grep safeno)"
+
+echo "$SAIDA_CAT"
