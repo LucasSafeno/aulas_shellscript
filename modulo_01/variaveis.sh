2f4f52caba1317873399b80bea773e0859a6385a
Lucas Safeno <lugnorio@gmail.com>
2025-08-13 19:35:14 -0300

    #A06 006 Variaveis reservadas

--- modulo_01/variaveis.sh
+++ modulo_01/variaveis.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env/bash
 
-NOME="Lucas
+# NOME="Lucas
 
-Tenório"
+# Tenório"
 
-NUMERO_1=24
-NUMERO_2=45
+# NUMERO_1=24
+# NUMERO_2=45
 
-TOTAL=$((NUMERO_1+NUMERO_2))
+# TOTAL=$((NUMERO_1+NUMERO_2))
 
-SAIDA_CAT="$(cat /etc/passwd | grep safeno)"
+# SAIDA_CAT="$(cat /etc/passwd | grep safeno)"
 
-echo "$SAIDA_CAT"
+# echo "$SAIDA_CAT"
+
+echo "Parametro 1: $1"
+echo "Parametro 2: $2"
+
+echo "Todos os parametros $*"
+
+echo "Quantos parametros : $#"
+
+echo "Saida do ultimo comando $?"
+
+echo "PID: $$"
+
+echo $0
